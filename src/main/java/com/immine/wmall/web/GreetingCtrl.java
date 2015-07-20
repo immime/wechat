2e860453d0467c67d9d2fff04034302379a0dfaf
John Wen <johnwenblabla@gmail.com>
2015-07-20 14:35:23 +0800

    test jrebel integrated

--- src/main/java/com/immine/wmall/web/GreetingCtrl.java
+++ src/main/java/com/immine/wmall/web/GreetingCtrl.java
@@ -5,7 +5,6 @@ import java.util.concurrent.atomic.AtomicLong;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -18,13 +17,13 @@ public class GreetingCtrl {
 	
 	private static final Logger logger = LoggerFactory.getLogger(GreetingCtrl.class);
 
-	private static final String template = "Hello, %s!";
+	private static final String template = "Hello aaa, %s!";
 	private final AtomicLong counter = new AtomicLong();
 
 	@RequestMapping
 	public @ResponseBody Greeting sayHello(
 			@RequestParam(value = "name", required = false, defaultValue = "Stranger") String name) {
-		logger.debug("excute count %s", counter.incrementAndGet());
+		logger.debug(String.format("excute count %s", counter.incrementAndGet()));
 		return new Greeting(counter.incrementAndGet(), String.format(template, name));
 	}
 
